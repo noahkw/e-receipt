e3db735795d926fa42513f972ad6de35fea822c5
Noah <noah@dreckbu.de>
2024-08-23 23:25:03 +0200

    Run prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import {ReceiptUpload} from "./components/ReceiptUpload.tsx";
+import { ReceiptUpload } from "./components/ReceiptUpload.tsx"
 
 function App() {
-
-    return (
-        <>
-            <div>test</div>
-            <ReceiptUpload></ReceiptUpload>
-        </>
-    )
+  return (
+    <>
+      <div>test</div>
+      <ReceiptUpload></ReceiptUpload>
+    </>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
-import * as pdfJsLib from 'pdfjs-dist'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import * as pdfJsLib from "pdfjs-dist"
+import "@fontsource/roboto/300.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
+import "./index.css"
+import App from "./App.tsx"
 
-pdfJsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.5.136/pdf.worker.min.mjs';
+pdfJsLib.GlobalWorkerOptions.workerSrc =
+  "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.5.136/pdf.worker.min.mjs"
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <App/>
-    </StrictMode>,
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
